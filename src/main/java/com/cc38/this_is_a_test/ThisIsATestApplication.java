21b7e85836ae61f129c8b82803472f17f68f3543
Justin-K-Ellis <jlke45@gmail.com>
2025-05-23 09:24:46 +0900

    refactor: delete unused code
    The code to do automatic server restarts was not working (this seems
    to be a known issues), so it was removed.

--- src/main/java/com/cc38/this_is_a_test/ThisIsATestApplication.java
+++ src/main/java/com/cc38/this_is_a_test/ThisIsATestApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ThisIsATestApplication {
 
 	public static void main(String[] args) {
-		System.setProperty("spring.devtools.restart.enabled", "true");
 		SpringApplication.run(ThisIsATestApplication.class, args);
 	}
 
